74a6ce0e7623145a9d7cb39b72f01cbf78bb7642
Claire Woods <clairewoods@gmail.com>
2024-10-28 14:12:52 -0700

    update hash-query

--- editor/src/components/header/components/ButtonCopyExternalLink.tsx
+++ editor/src/components/header/components/ButtonCopyExternalLink.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import { useMetaframeUrl } from '/@/hooks/useMetaframeUrl';
 
 import {
@@ -8,11 +9,28 @@ import {
   useToast,
 } from '@chakra-ui/react';
 import { Link } from "@phosphor-icons/react";
+import {
+  setHashValueInHashString,
+} from '@metapages/hash-query';
 
 export const ButtonCopyExternalLink: React.FC = () => {
   const { url } = useMetaframeUrl();
+  const [urlForCopy, setUrlForCopy] = useState('');
+
+  useEffect(() => {
+    if (!url) return;
+    const isLocal = window.location.hostname.includes('localhost');
+    let newUrl = setHashValueInHashString(url, 'edit', 'false')
+    if (isLocal) {
+      // TODO: swap localhost in for url val, useMetaframeUrl uses env variables to construct the path
+      setUrlForCopy(newUrl);
+    } else {
+      setUrlForCopy(newUrl);
+    }
+  }, [url])
+
   const toast = useToast();
-  const { onCopy } = useClipboard(url);
+  const { onCopy } = useClipboard(urlForCopy);
 
   return (
     <Box position="relative" display="inline-block">
